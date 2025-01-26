af5e46d79658cbf6a3c67ffd14270191ca9eb021
Tobiasz Bilinski <kysthalden@gmail.com>
2025-01-26 10:06:31 +0100

    Fix formatting, remove storage states

--- tests/task-tests-data.ts
+++ tests/task-tests-data.ts
@@ -1,20 +1,20 @@
 export default {
-    en: {
-        localeStrings: {
-            shop: 'Shop',
-            emptyCart: '0 Items',
-        },
-        data: {
-            itemSKU: 'ploom-x-advanced',
-        },
+  en: {
+    localeStrings: {
+      shop: "Shop",
+      emptyCart: "0 Items",
     },
-    pl: {
-        localeStrings: {
-            shop: 'Sklep',
-            emptyCart: 'Ilość produktów: 0',
-        },
-        data: {
-            itemSKU: '16199177',
-        },
+    data: {
+      itemSKU: "ploom-x-advanced",
     },
+  },
+  pl: {
+    localeStrings: {
+      shop: "Sklep",
+      emptyCart: "Ilość produktów: 0",
+    },
+    data: {
+      itemSKU: "16199177",
+    },
+  },
 };
